adc090a241a9af3e0a20477fc7ad1a6fc206cdcd
Maya Rachmani <maya3rm@gmail.com>
2022-04-11 04:45:52 +0300

    added alerts

--- src/layouts/notifications/index.js
+++ src/layouts/notifications/index.js
@@ -1,37 +1,20 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState } from "react";
 
-// @mui material components
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-
-// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDAlert from "components/MDAlert";
 import MDButton from "components/MDButton";
 import MDSnackbar from "components/MDSnackbar";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import notificationData from "./notificationData";
 
 function Notifications() {
+  const { alerts } = notificationData;
+
   const [successSB, setSuccessSB] = useState(false);
   const [infoSB, setInfoSB] = useState(false);
   const [warningSB, setWarningSB] = useState(false);
@@ -45,22 +28,6 @@ function Notifications() {
   const closeWarningSB = () => setWarningSB(false);
   const openErrorSB = () => setErrorSB(true);
   const closeErrorSB = () => setErrorSB(false);
-
-  const alertContent = (name) => (
-    <MDTypography variant="body2" color="white">
-      A simple {name} alert with{" "}
-      <MDTypography
-        component="a"
-        href="#"
-        variant="body2"
-        fontWeight="medium"
-        color="white"
-      >
-        an example link
-      </MDTypography>
-      . Give it a click if you like.
-    </MDTypography>
-  );
 
   const renderSuccessSB = (
     <MDSnackbar
@@ -127,30 +94,13 @@ function Notifications() {
                 <MDTypography variant="h5">Alerts</MDTypography>
               </MDBox>
               <MDBox pt={2} px={2}>
-                <MDAlert color="primary" dismissible>
-                  {alertContent("primary")}
-                </MDAlert>
-                <MDAlert color="secondary" dismissible>
-                  {alertContent("secondary")}
-                </MDAlert>
-                <MDAlert color="success" dismissible>
-                  {alertContent("success")}
-                </MDAlert>
-                <MDAlert color="error" dismissible>
-                  {alertContent("error")}
-                </MDAlert>
-                <MDAlert color="warning" dismissible>
-                  {alertContent("warning")}
-                </MDAlert>
-                <MDAlert color="info" dismissible>
-                  {alertContent("info")}
-                </MDAlert>
-                <MDAlert color="light" dismissible>
-                  {alertContent("light")}
-                </MDAlert>
-                <MDAlert color="dark" dismissible>
-                  {alertContent("dark")}
-                </MDAlert>
+                {alerts.map((alert) => (
+                  <MDAlert color={alert.color} dismissible>
+                    <MDTypography variant="body2" color="white">
+                      {alert.content}
+                    </MDTypography>
+                  </MDAlert>
+                ))}
               </MDBox>
             </Card>
           </Grid>
@@ -159,14 +109,6 @@ function Notifications() {
             <Card>
               <MDBox p={2} lineHeight={0}>
                 <MDTypography variant="h5">Notifications</MDTypography>
-                <MDTypography
-                  variant="button"
-                  color="text"
-                  fontWeight="regular"
-                >
-                  Notifications on this page use Toasts from Bootstrap. Read
-                  more details here.
-                </MDTypography>
               </MDBox>
               <MDBox p={2}>
                 <Grid container spacing={3}>
---
+++ src/layouts/notifications/notificationData.js
@@ -0,0 +1,24 @@
+export default {
+  alerts: [
+    {
+      id: 1,
+      color: "primary",
+      content: " צוות מספר אחד זקוק למתכנת נוסף - senior.",
+    },
+    {
+      id: 2,
+      color: "secondary",
+      content: "בן אדם שתיים לא מרוצה מהשירות שלו בצוות שלוש",
+    },
+    {
+      id: 3,
+      color: "success",
+      content: "עידו משתחרר בקרוב. יש לדאוג לחפיפת אדם חדש בצוות מספר ארבע",
+    },
+    {
+      id: 4,
+      color: "error",
+      content: "יועד לא כל כך",
+    },
+  ],
+};
